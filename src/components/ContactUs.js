2f9b864cf187de7d69be6e8b525b3a226eb9e1d8
Raishan <raishanbernard@gmail.com>
2022-05-10 16:42:00 +0000

    minor changes to the footer

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,7 +7,7 @@ export default class ContactUs extends Component {
           <div className="row section-head">
             <div className="ten columns">
               <p className="lead">
-              Feel free to contact me for any work.
+              Feel free to contact me for any inquiries.
               </p>
             </div>
           </div>
